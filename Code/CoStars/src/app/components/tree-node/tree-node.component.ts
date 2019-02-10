9f37816ce5acbd62c5ae02e45756acb59eacbf46
Jeremy Brachle <jeremybrachle@gmail.com>
2019-02-09 19:16:56 -0600

    Updated functionality for adding and deleting nodes to tree

--- Code/CoStars/src/app/components/tree-node/tree-node.component.ts
+++ Code/CoStars/src/app/components/tree-node/tree-node.component.ts
@@ -22,14 +22,41 @@ export class TreeNodeComponent implements OnInit {
   }
 
   addNode() {
-    // push to the game tree
-    this.gameTree.push(this.currNode);
+    // logic for adding to the game tree:
+    // make the current last element no longer the last
+    this.gameTree[this.gameTree.length - 1].isLast = false;
+
+    // build the new node to be added:
+    // new type will be the opposite of the previous type
+    let newType = '';
+    if (this.gameTree[this.gameTree.length - 1].type === 'actor') {
+      newType = 'film';
+    } else {
+      newType = 'actor';
+    }
+
+    // create the new node object
+    let newNode = new Node('new node', newType, false, true);
+
+    // push the newly build node
+    this.gameTree.push(newNode);
 
   }
 
   deleteNode() {
     // pop dat node of da tree yo
-    this.gameTree.pop();
+    // this will pop the click node and any node below it
+    // check to see how many nodes will be popped
+    // (number of pops) = (array size) - (index of current node)
+    let pops = (this.gameTree.length) - (this.gameTree.indexOf(this.currNode));
+
+    // loop and pop the nodes
+    for (let i = 0; i < pops; i++) {
+      this.gameTree.pop();
+    }
+
+    // now assign the bottom most node as the last
+    this.gameTree[this.gameTree.length - 1].isLast = true;
   }
 
 }
